8774bb499f5cd34ea9315faf9b9be2bff5043e80
Seongbin Kim <seongbin9786@gmail.com>
2026-07-05 18:03:42 +0900

    chore: husky, motion, ts-pattern, vitest, testing-library, code-inspector-plugin, svgr

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import { devtools } from '@tanstack/devtools-vite'
 
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import svgr from 'vite-plugin-svgr'
+import { codeInspectorPlugin } from 'code-inspector-plugin'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
+    codeInspectorPlugin({ bundler: 'vite' }),
     devtools(),
     tailwindcss(),
     tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+    svgr(),
     viteReact(),
   ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test-setup.ts'],
+  },
 })
 
 export default config
